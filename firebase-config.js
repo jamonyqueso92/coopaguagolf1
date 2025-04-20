83e04bab9afd92011571a1c84a683ec28041e225
jamonyqueso92 <jamonyroquefort@gmail.com>
2025-04-20 08:37:01 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,25 +1,13 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyA3EwtirsHI_kwxMLH5SP2rXbm3vCYpqhg",
+  authDomain: "coopaguagolf-b1e2f.firebaseapp.com",
+  projectId: "coopaguagolf-b1e2f",
+  storageBucket: "coopaguagolf-b1e2f.firebasestorage.app",
+  messagingSenderId: "980063915524",
+  appId: "1:980063915524:web:e991b552b607d2447aef51",
+  measurementId: "G-V6Z34N9V06"
+};
 
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyA3EwtirsHI_kwxMLH5SP2rXbm3vCYpqhg",
-    authDomain: "coopaguagolf-b1e2f.firebaseapp.com",
-    projectId: "coopaguagolf-b1e2f",
-    storageBucket: "coopaguagolf-b1e2f.firebasestorage.app",
-    messagingSenderId: "980063915524",
-    appId: "1:980063915524:web:e991b552b607d2447aef51",
-    measurementId: "G-V6Z34N9V06"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
